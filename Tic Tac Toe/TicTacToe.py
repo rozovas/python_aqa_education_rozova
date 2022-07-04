441fcb9eaa252f73fba4d0a46854c2a5db8f612a
Svitlana <svitlana.rozova@gmail.com>
2022-07-04 22:39:23 +0300

    added docstrings, logging, removed prints from display_field func

--- Tic Tac Toe/TicTacToe.py
+++ Tic Tac Toe/TicTacToe.py
@@ -1,91 +1,20 @@
 import sys
+import logging
 
+# creating a custom logger:
+logger = logging.getLogger("TicTacToe")
 
-class Game:
+# creating and managing handlers:
+f_handler = logging.FileHandler('/Tic Tac Toe/game.log')
+s_handler = logging.StreamHandler()
+f_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+s_handler.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s'))
+f_handler.setLevel(logging.INFO)
+s_handler.setLevel(logging.INFO)
 
-    def __init__(self, field=None):
-        if field is None:
-            field = [['_'] * 3, ['_'] * 3, ['_'] * 3]
-        self.field = field
-        self.x_player = True
-
-    def display_field(self):
-        print("---------")
-        for row in self.field:
-            print(f"| {row[0]} {row[1]} {row[2]} |")
-        print("---------")
-
-    @staticmethod
-    def define_winner(i):
-        if i[0] == i[1] == i[2] == "X":
-            return 1  # "X wins"
-        elif i[0] == i[1] == i[2] == "0":
-            return 2  # "0 wins"
-        else:
-            return 0  # "Draw"
-
-    def divide_field(self):
-        h = self.field  # [list(s[0]), list(s[1]), list([2])]
-        v = [[self.field[0][0], self.field[1][0], self.field[2][0]],
-             [self.field[0][1], self.field[1][1], self.field[2][1]],
-             [self.field[0][2], self.field[1][2], self.field[2][2]]]
-        d = [[self.field[0][0], self.field[1][1], self.field[2][2]],
-             [self.field[0][2], self.field[1][1], self.field[2][0]]]
-        return h, v, d
-
-    def get_winner(self):
-        h, v, d = self.divide_field()
-        l = []
-        x = h[0].count('X') + h[1].count('X') + h[2].count('X')
-        o = h[0].count('0') + h[1].count('0') + h[2].count('0')
-        for i in [*h, *v, *d]:
-            l.append(self.define_winner(i))
-        if (l.count(1) >= 1 and l.count(2) >= 1) or (abs(x - o) > 1):
-            # Impossible
-            return 3
-        elif l.count(1) == 1:
-            # X wins
-            return 1
-        elif l.count(2) == 1:
-            # 0 wins
-            return 2
-        elif "_" in [*self.field]:
-            # Draw
-            return 0
-
-    def accept_coordinates(self, x, y, char):
-        x = int(x)
-        y = int(y)
-        if not (0 < x < 4 and 0 < y < 4):
-            raise CellNumberError
-            # TODO: rise exception
-        elif self.field[x - 1][y - 1] != '_':
-            return "The cell is occupied! Choose another one!"
-        else:
-            if char == 'X':
-                if self.get_winner() is None:
-                    self.field[x - 1][y - 1] = 'X'
-            else:
-                if self.get_winner() is None:
-                    self.field[x - 1][y - 1] = '0'
-
-
-class Player:
-
-    # def __new__(cls, **kwargs):
-    #     ...
-
-    def __init__(self, name):
-        self.name = name
-        self.win_log = 0
-        # self.age = age
-
-    def __str__(self):
-        return self.name
-
-    def win_log_update(self):
-        self.win_log += 1
-        return self.win_log
+# adding handlers to a logger:
+logger.addHandler(f_handler)
+logger.addHandler(s_handler)
 
 
 class CellNumberError(Exception):
@@ -105,45 +34,158 @@ class CellOccupiedError(Exception):
     def __str__(self):
         return self.message
 
-#
-# def another_game():
-#     ...
+
+class Game:
+    """Class game containing game logic"""
+
+    def __init__(self, field=None):
+        if field is None:
+            field = [['_'] * 3, ['_'] * 3, ['_'] * 3]
+        self.field = field
+        self.x_player = True
+
+    def display_field(self):
+        """Returns current field state that can be printed"""
+        def mutable_part():
+            f2 = []
+            for row in self.field:
+                f1 = f"| {row[0]} {row[1]} {row[2]} |\n"
+                f2.append(f1)
+            return f2[0] + f2[1] + f2[2]
+
+        frame = "_________\n"
+        return frame + mutable_part() + frame
 
 
+    @staticmethod
+    def define_winner(i):
+        """Defines winner taking a row of 3 characters as argument"""
+        if i[0] == i[1] == i[2] == "X":
+            return 1  # "X wins"
+        elif i[0] == i[1] == i[2] == "0":
+            return 2  # "0 wins"
+        else:
+            return 0  # "Draw"
+
+    def divide_field(self):
+        """Divides current field into rows from a direction point of view: horizontal, vertical, diagonal
+        which helps in further field manipulations and calculations"""
+        h = self.field  # [list(s[0]), list(s[1]), list([2])]
+        v = [[self.field[0][0], self.field[1][0], self.field[2][0]],
+             [self.field[0][1], self.field[1][1], self.field[2][1]],
+             [self.field[0][2], self.field[1][2], self.field[2][2]]]
+        d = [[self.field[0][0], self.field[1][1], self.field[2][2]],
+             [self.field[0][2], self.field[1][1], self.field[2][0]]]
+        return h, v, d
+
+    def get_winner(self):
+        """Determines if somebody won depending on the current state of the field"""
+        h, v, d = self.divide_field()
+        l = []
+        x = h[0].count('X') + h[1].count('X') + h[2].count('X')
+        o = h[0].count('0') + h[1].count('0') + h[2].count('0')
+        for i in [*h, *v, *d]:
+            l.append(self.define_winner(i))
+        if (l.count(1) >= 1 and l.count(2) >= 1) or (abs(x - o) > 1):
+            # Impossible
+            return 3
+        elif l.count(1) == 1:
+            # X wins
+            return 1
+        elif l.count(2) == 1:
+            # 0 wins
+            return 2
+        elif "_" in [*self.field]:
+            # Draw
+            # TODO: think if Draw can be determined in some other way
+            return 0
+
+    def accept_coordinates(self, x, y, char):
+        """Accepts coordinates and the character of the player (supposed to be X or 0),
+        checks if the cell is not yet occupied, and if there is no winner yet, changes the field accordingly"""
+        x = int(x)
+        y = int(y)
+        if not (0 < x < 4 and 0 < y < 4):
+            raise CellNumberError()
+        elif self.field[x - 1][y - 1] != '_':
+            raise CellOccupiedError
+        else:
+            if self.get_winner() is None:
+                if char == 'X':
+                    self.field[x - 1][y - 1] = 'X'
+                elif char == '0':
+                    self.field[x - 1][y - 1] = '0'
+
+
+class Player:
+    """Used to create a player of a game. Has the only attribute of name"""
+
+    # def __new__(cls, **kwargs):
+    #     ...
+
+    def __init__(self, name):
+        self.name = name
+        self.win_log = 0
+        # self.age = age
+
+    def __str__(self):
+        return self.name
+
+    def win_log_update(self):
+        self.win_log += 1
+        return self.win_log
+
+
+# client part
+
+
+def game_time(func):
+    """decorator that allows to calculate and log time spent for one game session"""
+    import time
+
+    def wrapper(*args):
+        start = time.time()
+        func(*args)
+        end = time.time()
+        t = end - start
+        logging.debug(f"Game lasted for {t} seconds")
+    return wrapper
+
+
+@game_time
 def start_game(player_x, player_0):
     current_player = player_x
     game = Game()
-    game.display_field()
-    game_finished = 0
+    print(game.display_field())
     while True:
         try:
-            x, y = input(f"{current_player.name}, insert coordinates: ").split()
+            x, y = input(f"{current_player}, insert coordinates: ").split()
             # x, y = i[int(el) for el in input("Insert coordinates: ").split()]
             game.accept_coordinates(x, y, 'X' if current_player == player_x else '0')
             current_player = player_x if current_player == player_0 else player_0
-            game.display_field()
+            print(game.display_field())
             winner = game.get_winner()
             if winner is not None:
                 if winner == 1:
-                    print(f"{player_x.name} wins. Congrats!")
+                    logging.info(f"{player_x.name} wins. Congrats!")
                     return
                 elif winner == 2:
-                    print(f"{player_0.name} wins. Well done!")
+                    logging.info(f"{player_0.name} wins. Well done!")
                     return
                 elif winner == 0:
-                    print("Draw")
+                    logging.info("Draw")
                     return
                 elif winner == 3:
-                    print("Impossible")
+                    logging.info("Impossible")
 
         except ValueError:
-            print("You should enter two numbers separated by space!")
+            logging.error("You should enter two numbers separated by space!")
 
         except CellNumberError as c:
-            print(c)
+            logging.error(c)
 
         except CellOccupiedError as o:
-            print(o)
+            logging.error(o)
 
 
 def main():
@@ -189,5 +231,4 @@ def main():
     #     play_game(player_x, player_0)
 
 
-if __name__ == "__main__":
-    main()
+main()
